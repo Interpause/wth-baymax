d8540e369fa55e2c8c988f6bad0c3aa648f69169
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-12-21 13:36:08 +0000

    feat: basic auth security

--- src/app.py
+++ src/app.py
@@ -3,11 +3,14 @@
 import asyncio
 import logging
 import os
+import secrets
 import time
 from pathlib import Path
+from uuid import uuid4
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, UploadFile
+from fastapi import Depends, FastAPI, HTTPException, UploadFile
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from .model import AudioCache, ModelManager, Msgs
 
@@ -25,15 +28,30 @@ def create_app():
     """
     model_path = os.getenv("MODEL_PATH")
     audio_cache_path = os.getenv("AUDIO_CACHE_PATH")
+    token = os.getenv("TOKEN") or uuid4().hex
     Path(audio_cache_path).mkdir(parents=True, exist_ok=True)
     assert Path(model_path).exists(), "Model path does not exist."
 
-    app = FastAPI()
+    log.info(f"User: user, Token: {token}")
+    security = HTTPBasic()
+
+    def _require_login(creds: HTTPBasicCredentials = Depends(security)):
+        if secrets.compare_digest(
+            creds.password.encode("utf-8"), token.encode("utf-8")
+        ):
+            return True
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    app = FastAPI(dependencies=[Depends(security)])
     audio_cache = AudioCache(audio_cache_path)
     model_manager = ModelManager(model_path, audio_cache)
 
     @app.get("/hello")
-    async def hello():
+    async def hello(_=Depends(_require_login)):
         """Returns a greeting.
 
         Returns:
@@ -45,13 +63,13 @@ def create_app():
         return {"message": "Hello, World!"}
 
     @app.post("/upload_audio/")
-    async def upload_audio(file: UploadFile):
+    async def upload_audio(file: UploadFile, _=Depends(_require_login)):
         """Uploads an audio clip."""
         audio_id = await asyncio.to_thread(audio_cache.save, file.file)
         return {"audio_id": audio_id}
 
     @app.post("/generate")
-    async def generate(msgs: Msgs):
+    async def generate(msgs: Msgs, _=Depends(_require_login)):
         """Generates audio from text."""
         start = time.monotonic()
         resp = await asyncio.to_thread(model_manager.generate, msgs)
